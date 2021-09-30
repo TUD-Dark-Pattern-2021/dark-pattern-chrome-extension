cdb64f2b8763b5c1c254904a9e0ab39de3c14e56
Yufei <wstcsyf@gmail.com>
2021-09-30 03:00:45 +0100

    change some class name with html

--- js/sidewindow.js
+++ js/sidewindow.js
@@ -1,37 +1,38 @@
 //checking/Reporting
 $(function() {
-    $('#div1').find('input').click(function() {
-        $('#div1').find('input').attr('class', '');
-        $('.contentContainer').find('.showpage').css('display', 'none');
+    $('#DP-div1').find('input').click(function() {
+        $('#DP-div1').find('input').attr('class', '');
+        $('.DP-contentContainer').find('.DP-showpage').css('display', 'none');
         $(this).attr('class', 'active');
-        $('.contentContainer').find('.showpage').eq($(this).index()).css('display', 'block');
+        $('.DP-contentContainer').find('.DP-showpage').eq($(this).index()).css('display', 'block');
+
     })
 })
 
 //detection list
 $(function (){
-    $("#accordion>div>p").click( function (){
-        if($(".dpMenu").css("display")==="none"){
-            $(".dpMenu").css('display', 'block')
+    $("#DP-accordion>div>p").click( function (){
+        if($(".DP-dpMenu").css("display")==="none"){
+            $(".DP-dpMenu").css('display', 'block')
             $(this).nextAll().slideDown().end().parent().siblings().children("div");
         }else{
-            $(".dpMenu").css('display', 'none')
-       }
+            $(".DP-dpMenu").css('display', 'none')
+        }
 
     })
 })
 
 //check box
 $('input[name="checkItem"]').change(function(){
-    $("#box2").prop('checked',true);
+    $("box2").prop('checked',true);
 })
 //textResult & Piechart
 $(function() {
-    $('#result-area-button').find('input').click(function() {
-        $('#result-area-button').find('input').attr('class', '');
-        $('#result-area').find('.area').css('display', 'none');
-        $(this).attr('class', 'active_result');
-        $('#result-area').find('.area').eq($(this).index()).css('display', 'block');
+    $('#DP-result-area-button').find('input').click(function() {
+        $('#DP-result-area-button').find('input').attr('class', '');
+        $('#DP-result-area').find('.DP-area').css('display', 'none');
+        $(this).attr('class', 'DPactive_result');
+        $('#DP-result-area').find('.DP-area').eq($(this).index()).css('display', 'block');
     })
 })
 
