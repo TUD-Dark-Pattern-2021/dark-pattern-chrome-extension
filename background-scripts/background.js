39f75196c80a3accd34f97f0b948fe360988d006
ADowley <dowleyalan@gmail.com>
2021-10-18 12:30:12 +0100

    connected popup with background.js, so calls can be made to node server

--- background-scripts/background.js
+++ background-scripts/background.js
@@ -1,39 +1,63 @@
-//sets a listener onto the extension icon and listens till the icon is clicked. Once clicked the 'tab' object gives the current tab that is open and the id fo the current tab can be gotten.
-//sends a message to the content script once the icon is clicked. Content scripts answers with a reply, which gets console logged.
+// // sets a listener onto the extension icon and listens till the icon is clicked. Once clicked the 'tab' object gives the current tab that is open and the id fo the current tab can be gotten.
+// // sends a message to the content script once the icon is clicked. Content scripts answers with a reply, which gets console logged.
 // chrome.action.onClicked.addListener((tab) => {
-//     chrome.tabs.sendMessage(
-//         tab.id, { callFunction: "ControllerFunction" },
-//         function(response) {
-//             console.log(response);
-//         });
+//     chrome.tabs.sendMessage({ message: "getURL" }, function(response) {
+//         console.log(response);
+//     });
 // });
+
 
 //listener to listen for any message sent by scripts 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'Get Data') {
-        sendData(request.data);
-        sendResponse("Done");
+    if (request.message === 'GetData') {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { message: "GetHTML" }, function(response) {
+                sendData(response.data);
+                sendResponse("data gotten");
+            });
+        });
     }
+    return true;
 });
-
 
 //fucntion to get the data from the api running on the node server, and then sends the response data to another content script
 async function sendData(raw_html) {
     encoded_html = encodeURIComponent(raw_html);
     await fetch("http://dark-pattern-node-js-dev.eu-west-1.elasticbeanstalk.com/api/dp/detect", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        timeout: 0,
-        body: JSON.stringify({ "html": encoded_html }),
-    }).then((response) => {
-        return response.json();
-    }).then(data => {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, { message: "Data Gotten", data: data }, (response) => {
-                console.log(response);
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            timeout: 0,
+            body: JSON.stringify({ "html": encoded_html }),
+        }).then(response => response.json())
+        .then(data => {
+            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { message: "Data Gotten", data: data }, function(response) {
+                    console.log(response);
+                });
+                chrome.runtime.sendMessage({ message: "Data Retrieved", data: data }, function(response) {
+                    console.log(response);
+                });
             });
+
 
         })
 
-    });
 }
+
+
+
+//     .then((response) => {
+//         return response.json();
+//     }).then(data => {
+//         console.log(data);
+//         // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//         //     chrome.tabs.sendMessage(tabs[0].id, { message: "Data Gotten", data: data }, (response) => {
+//         //         console.log(response);
+//         //     });
+
+//         // })
+
+//     }).catch(error =>{
+//         console.log("there was an error");
+//     })
+// }
--- popup-scripts/MainWindow.js
+++ popup-scripts/MainWindow.js
@@ -1,6 +1,15 @@
+window.onload = function() {
+    chrome.tabs.query({ active: true, currentWindow: true },
+        function(tabs) {
+            let domain = new URL(tabs[0].url).hostname
+            document.getElementById("domain_name").innerHTML = domain;
+        });
+}
+
 document.getElementById("results").addEventListener("click", switchtab);
 document.getElementById("reports").addEventListener("click", switchtab);
 document.getElementById("settings").addEventListener("click", switchtab);
+document.getElementById("detection_button").addEventListener("click", getdata);
 
 function switchtab(e) {
     let active = document.getElementsByClassName("nav_list_active");
@@ -22,3 +31,17 @@ function switchtab(e) {
     }
 
 }
+
+function getdata() {
+    chrome.runtime.sendMessage({ message: "GetData" },
+        function(response) {
+            console.log(response);
+        });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'Data Retrieved') {
+        console.log(request.data);
+        sendResponse("Data arrived at Popup.js")
+    }
+});
