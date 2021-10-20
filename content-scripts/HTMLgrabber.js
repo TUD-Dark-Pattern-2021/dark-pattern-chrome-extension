51727e61f54ffc42479a77cf3a6b500f82aedd92
ADowley <dowleyalan@gmail.com>
2021-10-20 16:39:48 +0100

    just commenting and small bit more logic

--- content-scripts/HTMLgrabber.js
+++ content-scripts/HTMLgrabber.js
@@ -1,3 +1,9 @@
+//this listner function does a few things:
+// 1. grabs the HTML of the current webpage to send to node server
+// 2. gets the data from the session storage
+// 3. sets the returned data from the node server into the session storage of the webpage
+// 4. removes the data from the session storage
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.message == 'GetHTML') {
         sessionStorage.setItem("DarkPatternData_534724", "adsfasdfas");
